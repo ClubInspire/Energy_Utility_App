150778dab06dbe92ea4570031ca35bdade80a5cc
Paul Donor <classifiedrandomguy@gmail.com>
2015-01-10 16:05:25 -0800

    Create TryHarder.java

---
+++ TryHarder.java
@@ -0,0 +1,45 @@
+package test1.examples;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+
+public class TryHarder extends JFrame {
+
+    public TryHarder() {
+        initUI();
+    }
+
+    public final void initUI() {
+
+       JPanel panel = new JPanel();
+       getContentPane().add(panel);
+
+       panel.setLayout(null);
+
+       JButton quitButton = new JButton("Commit Sudoku");
+       quitButton.setBounds(50, 60, 140, 40);
+       quitButton.addActionListener(new ActionListener() {
+           public void actionPerformed(ActionEvent event) {
+               System.exit(0);
+          }
+       });
+
+       panel.add(quitButton);
+
+       setTitle("Commit Sudoku?");
+       setSize(300, 200);
+       setLocationRelativeTo(null);
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+           TryHarder ex = new TryHarder();
+           ex.setVisible(true);
+    }
+}
